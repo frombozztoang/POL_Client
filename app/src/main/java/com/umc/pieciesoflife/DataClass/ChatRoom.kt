ff219735796ac048c8e8262ed629891c26a7c961
Your Name <you@example.com>
2023-02-08 03:25:20 +0900

    FIREBASE JOIN ver1

--- app/src/main/java/com/umc/pieciesoflife/DataClass/ChatRoom.kt
+++ app/src/main/java/com/umc/pieciesoflife/DataClass/ChatRoom.kt
@@ -2,6 +2,6 @@ package com.umc.pieciesoflife.DataClass
 
 data class ChatRoom(
     val users: Map<String, Boolean>? = HashMap(),
-    var messages : Map<String, Message>? = HashMap(),
+    var messages : Map<String, ChatMessage>? = HashMap(),
 ) : java.io.Serializable{
 }
