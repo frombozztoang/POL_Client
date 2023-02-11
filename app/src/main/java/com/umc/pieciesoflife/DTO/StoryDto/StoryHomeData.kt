1414d3b643339048c1daa4a2e478f934cb8ccafb
alex-koko <bookshelf1234@ewhain.net>
2023-02-12 05:36:24 +0900

    Story DTO - postman 보고 수정

--- app/src/main/java/com/umc/pieciesoflife/DTO/StoryDto/StoryHomeData.kt
+++
@@ -1,8 +0,0 @@
-package com.umc.pieciesoflife.DTO.StoryDto
-
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-data class StoryHomeData(
-    @SerializedName("storyList") @Expose val storyList: List<StoryHomeStory>
-)
