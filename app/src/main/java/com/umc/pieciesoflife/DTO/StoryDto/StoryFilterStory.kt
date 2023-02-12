a4ca0494f7115a47490bca6bec0700fee5e6350d
alex-koko <bookshelf1234@ewhain.net>
2023-02-12 13:32:27 +0900

    Story Filter DTO 생성

---
+++ app/src/main/java/com/umc/pieciesoflife/DTO/StoryDto/StoryFilterStory.kt
@@ -0,0 +1,13 @@
+package com.umc.pieciesoflife.DTO.StoryDto
+
+data class StoryFilterStory(
+    val backgroundColor: String,
+    val date: String,
+    val description: String,
+    val id: Int,
+    val nickname: String,
+    val profileImgUrl: String,
+    val storyTag: String,
+    val title: String,
+    val userId: Int
+)
