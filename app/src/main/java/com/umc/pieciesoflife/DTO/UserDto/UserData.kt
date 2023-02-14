d3ad95c2f0fa5dd996f594695a7242d458e6b636
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2023-02-14 23:17:12 +0900

    [Chore] UserData DTO 변경

--- app/src/main/java/com/umc/pieciesoflife/DTO/UserDto/UserData.kt
+++ app/src/main/java/com/umc/pieciesoflife/DTO/UserDto/UserData.kt
@@ -3,6 +3,7 @@ package com.umc.pieciesoflife.DTO.UserDto
 import com.google.gson.annotations.SerializedName
 
 data class UserData(
+    @SerializedName("userId") val id: Int,
     @SerializedName("level") val level: Int,
     @SerializedName("nickname") val nickname: String,
     @SerializedName("profileImgUrl") val profileImgUrl: String,
