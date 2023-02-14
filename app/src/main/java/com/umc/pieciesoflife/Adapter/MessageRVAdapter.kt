3f2125e40cdbf4da5b1aa71d404e97a6ee82c450
alex-koko <bookshelf1234@ewhain.net>
2023-02-15 01:11:47 +0900

    승희 오류 주석처리하고 다시 푸시

--- app/src/main/java/com/umc/pieciesoflife/Adapter/MessageRVAdapter.kt
+++ app/src/main/java/com/umc/pieciesoflife/Adapter/MessageRVAdapter.kt
@@ -1,65 +1,65 @@
-package com.umc.pieciesoflife.Adapter
-
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import com.umc.pieciesoflife.R
-
-class MessageRVAdapter(private val context: Context) : RecyclerView.Adapter<MessageRVAdapter.ViewHolder>() {
-
-    //클릭 interface 정의
-    interface MyItemClickListener{
-        fun onItemClick(position: Int)
-    }
-    //클릭 리스너 선언
-    private lateinit var mItemClickListner: MyItemClickListener
-    //클릭 리스너 등록
-    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
-        mItemClickListner = itemClickListener
-    }
-
-
-    // 데이터 저장하는 변수
-    var datas = mutableListOf<Message>()
-
-    // 뷰 홀더가 처음 생성될 때
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_message,parent,false)
-        return ViewHolder(view)
-    }
-
-    // 리스트의 개수
-    override fun getItemCount(): Int = datas.size
-
-    // 재활용 및 값을 넣어주는 곳
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(datas[position])
-    }
-
-
-    // View와 Data를 연결시키는 함수
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        init{
-            //itemView의 OnItemClick 상속 및 초기화
-            itemView.setOnClickListener {
-                mItemClickListner.onItemClick(adapterPosition)
-            }
-        }
-
-        private val imgUserProfile: ImageView = itemView.findViewById(R.id.msg_userImg)
-        private val txtUserName: TextView = itemView.findViewById(R.id.msg_userName)
-        private val txtDate: TextView = itemView.findViewById(R.id.msg_date)
-        private val txtContent: TextView = itemView.findViewById(R.id.msg_content)
-
-        fun bind(item: Message) {
-            imgUserProfile.setImageResource(item.userImg)
-            txtUserName.text=item.userName
-            txtDate.text=item.date
-            txtContent.text =item.content
-        }
-    }
-}
+//package com.umc.pieciesoflife.Adapter
+//
+//import android.content.Context
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.ImageView
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
+//import com.umc.pieciesoflife.R
+//
+//class MessageRVAdapter(private val context: Context) : RecyclerView.Adapter<MessageRVAdapter.ViewHolder>() {
+//
+//    //클릭 interface 정의
+//    interface MyItemClickListener{
+//        fun onItemClick(position: Int)
+//    }
+//    //클릭 리스너 선언
+//    private lateinit var mItemClickListner: MyItemClickListener
+//    //클릭 리스너 등록
+//    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
+//        mItemClickListner = itemClickListener
+//    }
+//
+//
+//    // 데이터 저장하는 변수
+//    var datas = mutableListOf<Message>()
+//
+//    // 뷰 홀더가 처음 생성될 때
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_message,parent,false)
+//        return ViewHolder(view)
+//    }
+//
+//    // 리스트의 개수
+//    override fun getItemCount(): Int = datas.size
+//
+//    // 재활용 및 값을 넣어주는 곳
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        holder.bind(datas[position])
+//    }
+//
+//
+//    // View와 Data를 연결시키는 함수
+//    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        init{
+//            //itemView의 OnItemClick 상속 및 초기화
+//            itemView.setOnClickListener {
+//                mItemClickListner.onItemClick(adapterPosition)
+//            }
+//        }
+//
+//        private val imgUserProfile: ImageView = itemView.findViewById(R.id.msg_userImg)
+//        private val txtUserName: TextView = itemView.findViewById(R.id.msg_userName)
+//        private val txtDate: TextView = itemView.findViewById(R.id.msg_date)
+//        private val txtContent: TextView = itemView.findViewById(R.id.msg_content)
+//
+//        fun bind(item: Message) {
+//            imgUserProfile.setImageResource(item.userImg)
+//            txtUserName.text=item.userName
+//            txtDate.text=item.date
+//            txtContent.text =item.content
+//        }
+//    }
+//}
--- app/src/main/java/com/umc/pieciesoflife/Fragment/UserChatFragment.kt
+++ app/src/main/java/com/umc/pieciesoflife/Fragment/UserChatFragment.kt
@@ -13,18 +13,18 @@ import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.umc.pieciesoflife.Acitivity.ChatActivity
-import com.umc.pieciesoflife.Adapter.MessageRVAdapter
+//import com.umc.pieciesoflife.Adapter.MessageRVAdapter
 import com.umc.pieciesoflife.DataClass.ChatRoom
 import com.umc.pieciesoflife.databinding.FragmentUserMessageBinding
 
 class UserChatFragment : Fragment() {
     private lateinit var Binding: FragmentUserMessageBinding
-    private lateinit var messageAdapter: MessageRVAdapter
-
-    val database = Firebase.firestore
-    var path = "HJ_chatRoom"
-    val messageList: ArrayList<ChatRoom> = arrayListOf() // 리사이클러뷰용 리스트
-    val mylist : ArrayList<String> = arrayListOf() // '내가' 있는 모든 채팅방 이름
+//    private lateinit var messageAdapter: MessageRVAdapter
+//
+//    val database = Firebase.firestore
+//    var path = "HJ_chatRoom"
+//    val messageList: ArrayList<ChatRoom> = arrayListOf() // 리사이클러뷰용 리스트
+//    val mylist : ArrayList<String> = arrayListOf() // '내가' 있는 모든 채팅방 이름
 
 
 
@@ -34,31 +34,31 @@ class UserChatFragment : Fragment() {
     ): View? {
         Binding = FragmentUserMessageBinding.inflate(inflater, container, false)
 
-        var userId = 0 //intent.getExtra어쩌구
-        //firebase에서 내가 포함된 채팅방 조회
-        var collectionReference = database.collection(path)
-
-        var query = collectionReference.whereEqualTo("myId",userId)
-        //var doc1 : Collection<QueryDocumentSnapshot> = query.get().
-
-
-
-
-
-        //리사이클러뷰 어댑터
-        messageAdapter = MessageRVAdapter(Activity())
-        Binding.rvMessage.adapter = messageAdapter
-        Binding.rvMessage.layoutManager = LinearLayoutManager(context)
-
-        initRecycler()
-
-        //->ChatActivity
-        messageAdapter.setMyItemClickListener(object : MessageRVAdapter.MyItemClickListener{
-            override fun onItemClick(position: Int) {
-                val intent = Intent(context, ChatActivity::class.java)
-                startActivity(intent)
-            }
-        })
+//        var userId = 0 //intent.getExtra어쩌구
+//        //firebase에서 내가 포함된 채팅방 조회
+//        var collectionReference = database.collection(path)
+//
+//        var query = collectionReference.whereEqualTo("myId",userId)
+//        //var doc1 : Collection<QueryDocumentSnapshot> = query.get().
+//
+//
+//
+//
+//
+//        //리사이클러뷰 어댑터
+//        messageAdapter = MessageRVAdapter(Activity())
+//        Binding.rvMessage.adapter = messageAdapter
+//        Binding.rvMessage.layoutManager = LinearLayoutManager(context)
+//
+//        initRecycler()
+//
+//        //->ChatActivity
+//        messageAdapter.setMyItemClickListener(object : MessageRVAdapter.MyItemClickListener{
+//            override fun onItemClick(position: Int) {
+//                val intent = Intent(context, ChatActivity::class.java)
+//                startActivity(intent)
+//            }
+//        })
 
 
         return Binding.root
